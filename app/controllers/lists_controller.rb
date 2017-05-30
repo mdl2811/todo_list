c4faeb8b3733652bd1ea47dd6aa6d85152b020b6
Michael LaBahn <mdlabahn@yahoo.com>
2017-05-30 16:55:59 -0600

    borken

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,50 @@
+class ListsController < ApplicationController
+
+  def index
+    @lists = ToDoList.all
+  end
+
+  def show
+    @list = ToDoList.find(params[:id])
+  end
+
+  def new
+    @list = ToDoList.new
+  end
+
+  def create
+    # @list = ToDoList.new(list_params)
+    #   if @list.save
+    #     # got a new record in the database
+    #   redirect_to list_path(@list)
+    # else
+    #     # something went wrong, have user fix errors on the form
+    #   render :new
+    # end
+  end
+
+  def edit
+    @list = ToDoList.find(params[:id])
+  end
+
+  def update
+    # @list = ToDoList.find(params[:id])
+    # if @list.update(list_params)
+    #   redirect_to list_path(@list)
+    # else
+    #   render :edit
+    # end
+  end
+
+  def destroy
+  # @list = ToDoList.find(params[:id])
+  # @list.destroy
+  # redirect_to list_path
+  end
+
+  # private
+  #   def list_params
+  #     params.require(:list).permit(:title, :body)
+  #   end
+
+end
